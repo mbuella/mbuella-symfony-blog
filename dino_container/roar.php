ec8580c04c860a50498aed8fee7a0d67d7b7b144
Marlon Buella <marlon.buella@movent.com>
2017-10-17 11:58:26 +0800

    Apply container dumping/caching for faster execution

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -3,24 +3,42 @@
 namespace Dino\Play;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\Config\FileLocator;
 
 require __DIR__.'/../vendor/autoload.php';
 
 
-$container = new ContainerBuilder();
-$container->setParameter('root_dir', __DIR__);
+$start = microtime(true);
+$cachedContainer = __DIR__.'/cache/container.php';
+if (!file_exists($cachedContainer))
+{
+	$container = new ContainerBuilder();
+	$container->setParameter('root_dir', __DIR__);
 
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
-$loader->load('services.yml');
+	$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
+	$loader->load('services.yml');
 
+
+	$container->compile();
+	$dumper = new PhpDumper($container);
+	file_put_contents($cachedContainer, $dumper->dump());
+}
+
+require $cachedContainer;
+$container = new \ProjectServiceContainer();
 
 runApp($container);
 
-function runApp(ContainerBuilder $container)
+
+$elapsed = round((microtime(true) - $start) * 1000);
+$container->get('logger')->debug('Elapsed Time: '.$elapsed.'ms');
+
+function runApp(ContainerInterface $container)
 {
 	$container->get('logger')->info('ROOOOAR');	
 }
