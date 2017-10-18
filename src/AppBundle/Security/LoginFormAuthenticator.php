ba5b47a04d94fcf537f6ca6d6ed9136a2e035779
Marlon Buella <marlon.buella@movent.com>
2017-10-18 11:01:25 +0800

    Setup redirection logic for authentication

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -2,19 +2,28 @@
 namespace AppBundle\Security;
 
 use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
 	private $formFactory;
+	private $em;
+	private $router;
 
-	public function __construct(FormFactoryInterface $formFactory)
+	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, Router $router)
 	{
         $this->formFactory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
 	}
     public function getCredentials(Request $request)
     {
@@ -36,14 +45,35 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         $username = $credentials['_username'];
+		
+		return $this->em->getRepository('AppBundle:User')
+            ->findOneBy(['email' => $username]);
     }
     public function checkCredentials($credentials, UserInterface $user)
     {
+		// dump($user);die();
+		$password = $credentials['_password'];
+		
+		if ($password == 'iliketurtles') {
+            return true;
+        }
+        return false;
     }
     protected function getLoginUrl()
     {
+		return $this->router->generate('security_login');
     }
     protected function getDefaultSuccessRedirectUrl()
     {
+    }
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+		// $targetPath = $this->getTargetPath($request->getSession(), $providerKey);
+		
+		// if (!$targetPath) {
+             $targetPath = $this->router->generate('homepage');
+        // }
+ 
+        return new RedirectResponse($targetPath);
     }
 }
